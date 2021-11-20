7113406b80a18634356374e6b16f2f64ca34334c
Catherine Semiletova <catherine.semiletova@gmail.com>
2021-11-20 16:42:59 +0300

    Logic of following points is implemented.

---
+++ Learning Unity/Assets/Scenes/Lerp/EnemyFollowPath.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowPath : MonoBehaviour
+{
+    [SerializeField] private Transform _path;
+    [SerializeField] private float speed = 2f;
+
+    private Transform[] _pointsOfPath;
+    private int _currentPoint;
+    private Transform target;
+
+    void Start()
+    {
+        _pointsOfPath = new Transform[_path.childCount];
+        for (int i = 0; i < _path.childCount; i++)
+        {
+            _pointsOfPath[i] = _path.GetChild(i);
+        }
+        _currentPoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = _pointsOfPath[_currentPoint];
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if (transform.position == target.position)
+        {
+            _currentPoint++;
+            if (_currentPoint >= _pointsOfPath.Length)
+            {
+                _currentPoint = 0;
+            }
+        }
+    }
+}
