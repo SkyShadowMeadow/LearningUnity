74e4db004f30cd5a1cbbf2cc5b52ecf6c5eade11
Catherine Semiletova <catherine.semiletova@gmail.com>
2021-11-20 21:05:13 +0300

    Logic of the obstacle detection is implemented.

---
+++ Learning Unity/Assets/Project02(Cast)/ObstaclesDetector.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesDetector : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1f;
+    [SerializeField] private ContactFilter2D _contactFilter; 
+    private readonly RaycastHit2D[] _collisionResults = new RaycastHit2D[1];
+    private int _numberOfCollisions;
+    private Rigidbody2D _rigidbody2D;
+
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _numberOfCollisions = _rigidbody2D.Cast(Vector2.right, _contactFilter, _collisionResults, 10f);
+        if (_numberOfCollisions == 0)
+        {
+            _rigidbody2D.velocity = Vector2.right * _speed;
+        }
+        else
+        {
+            _rigidbody2D.velocity = new Vector2(0, 0);
+        }
+    }
+}
