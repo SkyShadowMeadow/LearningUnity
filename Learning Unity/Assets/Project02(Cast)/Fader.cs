e3c50c11c81b6f040c4eff4a920ec648b50eb58e
Catherine Semiletova <catherine.semiletova@gmail.com>
2021-11-22 19:57:58 +0300

    A coroutine to fade in is implemented.

---
+++ Learning Unity/Assets/Project02(Cast)/Fader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+
+    void Start()
+    {
+        var fadeInWork = StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        var currentColor = _image.color;
+        for (int i = 0; i < 255; i++)
+        {
+            float minus = 0.01f;
+            currentColor.a -= minus;
+            _image.color = currentColor;
+            yield return null;
+        }
+    }
+
+}
