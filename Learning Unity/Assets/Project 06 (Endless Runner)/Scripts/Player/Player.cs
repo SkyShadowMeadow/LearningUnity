4c22d3af3f93515a27225faa6f2905f04179a2ac
Catherine Semiletova <catherine.semiletova@gmail.com>
2021-12-01 20:48:56 +0300

    Health display has been added.

--- Learning Unity/Assets/Project 06 (Endless Runner)/Scripts/Player/Player.cs
+++ Learning Unity/Assets/Project 06 (Endless Runner)/Scripts/Player/Player.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.Events;
+
 namespace EndlessRunner
 {
     public class Player : MonoBehaviour
     {
+        public event UnityAction<int> HealthChanged;
+
         [SerializeField] private int _health;
 
+        private void Start()
+        {
+            HealthChanged?.Invoke(_health);
+        }
         public void ApplyDamage(int damage)
         {
             _health -= damage;
+            HealthChanged?.Invoke(_health);
+
             if (_health <= 0)
                 Die();
         }
